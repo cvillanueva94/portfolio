4827b7875dccd4a841c36a5102ada719de9c3e0e
Krlos Villanueva <krlosvilla101994@gmail.com>
2023-01-01 17:41:17 -0500

    new components

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <p class="hi text-white">Hi, my name is</p>
+    <h2 class="big-heading text-slate-400">Carlos Villanueva.</h2>
+    <h3 class="big-heading text-slate-300">I build things for the web.</h3>
+    <p className="text-slate-400">I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at Upstatement.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
